9208c5f8352c0aec0f06e8d5d4877f750eb6b7d7
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-01-12 14:42:53 +0100

    React receiver with some controls

--- receiver.js
+++ receiver.js
@@ -1,5 +1,8 @@
-import Firebase from 'firebase/lib/firebase-web'
-import Wad from './bower_components/Wad/src/wad'
+import React             from 'react'
+import ReactDOM          from 'react-dom'
+import Firebase          from 'firebase/lib/firebase-web'
+import receiverStyle     from './receiver.styl'
+import Wad               from './bower_components/Wad/src/wad'
 import { random, snare } from './instrument'
 
 let firebase = new Firebase('https://distributed-dub-step.firebaseio.com/instruments')
@@ -9,37 +12,17 @@ let seconds = 60
 let beat    = seconds/bpm
 let pattern = 8
 let allowedBeatSegments = [1,2.25,2.5,2.75,3.5,5,6.95]
-let remoteIntruments = {}
-
-firebase.on('child_added', (snap) => {
-    remoteIntruments[snap.key()] = {
-        wad : new Wad(snap.val())
-    }
-})
-firebase.on('child_removed', (snap) => {
-    delete remoteIntruments[snap.key()]
-})
-firebase.on('child_changed', (snap) => {
-    console.log('changed', snap.val())
-})
-
-
-let randomInstruments = []//random(30).map(i => {
-//    return new Wad(i)
-//})
 let beatBase = new Wad(snare)
-
 let pickRandomBeatSegment = () => {
     let skew = parseFloat(Math.random().toFixed(2))/10
     return allowedBeatSegments[Math.floor(Math.random()*allowedBeatSegments.length)]+skew
 }
-
 let playRandomInstrument = (inst) => {
+    if (!inst) return
     if (inst.source == 'square')
         inst.play({
             volume : 0.1,
             wait: beat * pickRandomBeatSegment(), 
-//                wait: beat * Math.random() * pattern,
             pitch : ['C5','C7'][Math.floor(Math.random()*2)]
         })
     else
@@ -50,29 +33,107 @@ let playRandomInstrument = (inst) => {
 
 Wad.setGlobalReverb({impulse : '/widehall.wav', wet : .5})
 
-let loop = () => {
-    // Base beat
-    beatBase.play({
-        wait: beat * 0
-    })
-    beatBase.play({
-        wait: beat * 2
-    })
-    beatBase.play({
-        wait: beat * 4
-    })
-    beatBase.play({
-        wait: beat * 6
-    })
-    // Randomize
-    Object.keys(remoteIntruments).forEach(id => {
-        let inst = remoteIntruments[id].wad
-        playRandomInstrument(inst)
-    })
-    randomInstruments.forEach((inst, index) => {
-        playRandomInstrument(inst)
-    })
+
+class DistributedDubStep extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            allowRemote : true,
+            randomInstruments : [],
+            remoteInstruments : []
+        }
+    }
+    render() {
+        return (
+            <div className="DistributedDubStep">
+                <style>{receiverStyle}</style>
+                <div className="controls">
+                    <div className="numRandom">
+                        Number of random: {this.state.randomInstruments.length}
+                        <button onClick={this.addRandom.bind(this)}>+</button>
+                        <button onClick={this.delRandom.bind(this)}>-</button>
+                    </div>
+                    <div className="numRemote">
+                        Number of remote: {this.state.remoteInstruments.length}
+                    </div>
+                    <div className="allowRemote">
+                        Allow remote instruments: {this.state.allowRemote ? 'Yes' : 'Nope'}
+                        <button onClick={this.toggleRemote.bind(this)}>Toggle</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    toggleRemote() {
+        this.setState({ allowRemote : !this.state.allowRemote })
+    }
+    addRandom() {
+        let rand = random(1)
+        let randomInstrument = {
+            wad : new Wad(rand[0])
+        }
+        this.state.randomInstruments.push(randomInstrument)
+        this.forceUpdate() 
+    }
+    delRandom() {
+        this.state.randomInstruments.pop()
+        this.forceUpdate()
+    }
+    addRemote(id, inst) {
+        let remoteInstrument = {
+            id  : id,
+            wad : new Wad(inst)
+        }
+        this.state.remoteInstruments.push(remoteInstrument)
+        this.forceUpdate() 
+    }
+    delRemote(id) {
+        this.state.remoteInstruments = this.state.remoteInstruments.filter(inst => {
+            return inst.id != id
+        })
+        console.log(this.state.remoteInstruments)
+        this.forceUpdate()
+    }
+    loop() {
+        // Base beat
+        beatBase.play({
+            wait: beat * 0
+        })
+        beatBase.play({
+            wait: beat * 2
+        })
+        beatBase.play({
+            wait: beat * 4
+        })
+        beatBase.play({
+            wait: beat * 6
+        })
+        // Randomize
+        this.state.randomInstruments.forEach(inst => {
+            playRandomInstrument(inst.wad)
+        })
+        // Randomize remote
+        if (!this.state.allowRemote) return
+        this.state.remoteInstruments.forEach(inst => {
+            playRandomInstrument(inst.wad)
+        })
+    }
+    componentDidMount() {
+        firebase.on('child_added', (snap) => {
+            this.addRemote(snap.key(), snap.val())
+        })
+        firebase.on('child_removed', (snap) => {
+            this.delRemote(snap.key())
+        })
+        firebase.on('child_changed', (snap) => {
+            console.log('changed', snap.val())
+        })
+        this.loop()
+        setInterval(this.loop.bind(this), Math.floor(beat * pattern * 1000))
+    }
 }
 
-loop()
-//setInterval(loop, Math.floor(beat * pattern * 1000))
+let container = document.createElement('div')
+document.body.appendChild(container)
+ReactDOM.render(<DistributedDubStep />, container)
+
