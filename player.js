dc8814f26bc5477d143eece1747f6dc3ce2b3211
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-01-12 13:34:13 +0100

    Added firebase and hooked up random instruments

--- player.js
+++ player.js
@@ -1,5 +1,7 @@
+import Firebase from 'firebase/lib/firebase-web'
 import { random } from './instrument'
 
+let firebase = new Firebase('https://distributed-dub-step.firebaseio.com/instruments')
 let instrument = random()
-
-
+let _instrument = firebase.push(instrument)
+_instrument.onDisconnect().remove()
--- receiver.js
+++ receiver.js
@@ -1,20 +1,51 @@
+import Firebase from 'firebase/lib/firebase-web'
 import Wad from './bower_components/Wad/src/wad'
 import { random, snare } from './instrument'
+
+let firebase = new Firebase('https://distributed-dub-step.firebaseio.com/instruments')
 
 let bpm     = 100
 let seconds = 60
 let beat    = seconds/bpm
 let pattern = 8
 let allowedBeatSegments = [1,2.25,2.5,2.75,3.5,5,6.95]
+let remoteIntruments = {}
 
-let randomInstruments = random(30).map(i => {
-    return new Wad(i)
+firebase.on('child_added', (snap) => {
+    remoteIntruments[snap.key()] = {
+        wad : new Wad(snap.val())
+    }
 })
+firebase.on('child_removed', (snap) => {
+    delete remoteIntruments[snap.key()]
+})
+firebase.on('child_changed', (snap) => {
+    console.log('changed', snap.val())
+})
+
+
+let randomInstruments = []//random(30).map(i => {
+//    return new Wad(i)
+//})
 let beatBase = new Wad(snare)
 
 let pickRandomBeatSegment = () => {
     let skew = parseFloat(Math.random().toFixed(2))/10
     return allowedBeatSegments[Math.floor(Math.random()*allowedBeatSegments.length)]+skew
+}
+
+let playRandomInstrument = (inst) => {
+    if (inst.source == 'square')
+        inst.play({
+            volume : 0.1,
+            wait: beat * pickRandomBeatSegment(), 
+//                wait: beat * Math.random() * pattern,
+            pitch : ['C5','C7'][Math.floor(Math.random()*2)]
+        })
+    else
+        inst.play({
+            wait: beat * pickRandomBeatSegment() 
+        })
 }
 
 Wad.setGlobalReverb({impulse : '/widehall.wav', wet : .5})
@@ -34,18 +65,12 @@ let loop = () => {
         wait: beat * 6
     })
     // Randomize
+    Object.keys(remoteIntruments).forEach(id => {
+        let inst = remoteIntruments[id].wad
+        playRandomInstrument(inst)
+    })
     randomInstruments.forEach((inst, index) => {
-        if (inst.source == 'square')
-            inst.play({
-                volume : 0.1,
-                wait: beat * pickRandomBeatSegment(), 
-//                wait: beat * Math.random() * pattern,
-                pitch : ['C5','C7'][Math.floor(Math.random()*2)]
-            })
-        else
-            inst.play({
-                wait: beat * pickRandomBeatSegment() 
-            })
+        playRandomInstrument(inst)
     })
 }
 
