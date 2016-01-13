64c44501919d7aea635bb5bc09753c89a2057010
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-01-13 10:34:24 +0100

    Baby shaker

--- player.js
+++ player.js
@@ -2,8 +2,13 @@ import React       from 'react'
 import ReactDOM    from 'react-dom'
 import Firebase    from 'firebase/lib/firebase-web'
 import Shake       from 'shake.js'
+import Svg         from '@asbjornenge/react-svg'
 import { random }  from './instrument'
 import playerStyle from './player.styl'
+import babysad     from './icons/babysad.svg'
+import babysmile   from './icons/babysmile.svg'
+import babyhappy   from './icons/babyhappy.svg'
+import babylove    from './icons/babylove.svg'
 
 let firebase = new Firebase('https://distributed-dub-step.firebaseio.com/instruments')
 let instrument = random()
@@ -12,7 +17,7 @@ _instrument.onDisconnect().remove()
 
 let myshake = new Shake({
     threshold: 10, // optional shake strength threshold
-    timeout: 1000 // optional, determines the frequency of event generation
+    timeout: 100 // optional, determines the frequency of event generation
 })
 myshake.start()
 
@@ -20,40 +25,49 @@ class DistributedDubStepPlayer extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            x : 0,
-            y : 0,
-            z : 0
+            shakeValue : 0
         }
     }
     render() {
-        let largestDelta = this.state.x
-        if (this.state.y > largestDelta) largestDelta = this.state.y
-        if (this.state.z > largestDelta) largestDelta = this.state.z
+        let largestDelta = this.state.shakeValue
+        let baby = babysad
+        if (largestDelta > 10) baby = babysmile
+        if (largestDelta > 30) baby = babyhappy
+        if (largestDelta > 50) baby = babylove
         return (
             <div className="DistributedDubStepPlayer">
                 <style>{playerStyle}</style>
-                <div className="shakeDelta">
-                    <div>{largestDelta}</div>
+                <div className="baby">
+                    <Svg svg={baby} />
                 </div>
             </div>
         )
     }
     shakeHandler(e) {
-        this.setState({
+        this.shakeDeltas.push({
             x : e.deltaX,
             y : e.deltaY,
             z : e.deltaZ
         })
         clearTimeout(this.shakeTimeout)
         this.shakeTimeout = setTimeout(() => {
-            this.setState({
-                x : 0,
-                y : 0,
-                z : 0
-            })
+            this.shakeDeltas = []
         },2000) 
     }
+    setShakeValues() {
+        let shakeValue = this.shakeDeltas.reduce((max, deltas) => {
+            let _max = deltas.x
+            if (deltas.y > _max) _max = deltas.y
+            if (deltas.z > _max) _max = deltas.z
+            if (_max > max) return _max
+            return max
+        },0)
+        this.shakeDeltas = []
+        this.setState({ shakeValue : shakeValue }) 
+    }
     componentDidMount() {
+        this.shakeDeltas = []
+        setInterval(this.setShakeValues.bind(this), 2000)
         window.addEventListener('shake', this.shakeHandler.bind(this), false)
     }
 }
